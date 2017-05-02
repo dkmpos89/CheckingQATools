abbf3e3edd71eb058adcb31f54584f53e30bc3e3
dkmpos89 <d_campos@hotmail.cl>
2017-05-02 17:12:02 -0300

    VFinal:02-05-2007

---
+++ bin/checkoutRequest.groovy
@@ -0,0 +1,119 @@
+import com.serena.dmclient.api.SystemAttributes;
+import com.serena.dmclient.api.DimensionsConnectionManager;
+import com.serena.dmclient.api.DimensionsConnection;
+import com.serena.dmclient.api.DimensionsConnectionDetails;
+import com.serena.dmclient.api.Filter;
+import com.serena.dmclient.api.DimensionsRelatedObject;
+import com.serena.dmclient.api.ItemRevision;
+
+import es.als.io.IOUtils;
+import com.serena.dmclient.api.Request;
+import com.serena.dmclient.api.Project;
+import com.serena.dmclient.api.Baseline;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class checkoutBaseline {
+
+	//private static Logger log = Logger.getLogger(checkoutBaseline.class);
+	private static String product = "";  						// Producto a analizar
+	private static String project = "";  						// Proyecto a analizar 
+	private static String br_object = ""; 						// linea base o request
+	private static String atrib = ""; 							// Atributo proyecto
+
+	private static String myItemslist = ""; 
+
+	private static String username = "dmsys";
+	private static String password = "BcoRipley12";
+	private static String dbname = "dimensions_adm";
+	private static String dbconn ="DIM12";
+	private static String ip = "192.168.10.66:48654";
+	private static DimensionsConnection connection = null;
+	
+	
+	// Metodo de emision de error durante el script
+	static void show(String message){
+		System.out.println(message);
+	}
+	static void failed(String message){
+		System.out.println(message);
+		show("\nPROCESO-TERMINADO-FAIL");
+		throw new Exception(message);
+	}
+	
+	public static void main(String[] args) throws IOException 
+	{
+		String executionPath = System.getProperty("user.dir");
+		List listItemReq;
+		//PropertyConfigurator.configure(executionPath+"/log4j.properties");
+		product = args[0];
+		project = args[1];
+		br_object = args[2];
+		atrib = args[3];
+
+		show("INICIANDO LA DESCARGA...");
+		show("---> Producto: "+product);
+		show("---> Proyecto: "+project);
+		show("---> Objeto: "+br_object);
+
+		
+		try {
+			// obteniendo la conexion con Dimensions
+			
+			DimensionsConnectionDetails details = new DimensionsConnectionDetails();
+			details.setUsername(username);
+			details.setPassword(password);
+			details.setDbName(dbname);
+			details.setDbConn(dbconn);
+			details.setServer(ip);
+			show("Conectando con Dimensions...");
+ 
+			connection = DimensionsConnectionManager.getConnection(details);		
+			if(connection!=null) 
+				show("---> Conexion exitosa");
+			else{
+				failed("[ No se puede conectar con Dimensions ]");
+			}
+			  
+			// Descarga de las ultimas revisiones de los items asociados
+			Request pkgRequest = connection.getObjectFactory().findRequest(br_object);
+			Project projectDimensions = connection.getObjectFactory().getProject(product + ':' + project)
+			// Filtro para identificar el baseline objetivo de la descarga
+			if (pkgRequest!= null && projectDimensions != null) {
+				Filter filter = new Filter();
+				filter.criteria().add(new Filter.Criterion(SystemAttributes.IS_LATEST_REV));
+				listItemReq = pkgRequest.getChildItems(filter, projectDimensions);
+			}else{
+				failed("[ Request: "+br_object+" no encontrado ]");
+			}
+
+
+			//solo deberia devolver un baseline 
+			if (listItemReq.size() == 0) 
+				failed("[ Request "+br_object+" vacio : "+listItemReq.size()+" ]");
+			else
+				show("---> Items encontrados: "+listItemReq.size());
+
+			if(listItemReq.size() >0 ){
+
+				System.out.println("[INICIO]");
+				for (DimensionsRelatedObject item : listItemReq) 
+				{
+					ItemRevision itemLastRevision  = (ItemRevision) item.getObject();
+					int[] queryAttributes = [ SystemAttributes.OBJECT_UID, SystemAttributes.STATUS, SystemAttributes.ITEMFILE_DIR, SystemAttributes.ITEMFILE_FILENAME ];
+					itemLastRevision.queryAttribute(queryAttributes);
+					List attributes = itemLastRevision.getAttribute(queryAttributes);
+					System.out.println(attributes.get(3).toString());
+				}
+
+				System.out.println("[FIN]");
+				show("PROCESO-TERMINADO-SUCCESS");
+			}else
+				failed("[ ERROR - Linea Base vacia ]");
+		}
+		finally {
+			// desconexion Dimensions			
+			if (connection != null) connection.close();
+		}
+	}
+}
--- mainwindow.h
+++ mainwindow.h
@@ -14,7 +14,9 @@
 #include <QUrl>
 #include <QDir>
 #include <QMediaPlayer>
-
+#include <QMap>
+#include <QMapIterator>
+#include <QTableWidget>
 
 namespace Ui {
 class MainWindow;
@@ -33,17 +35,19 @@ public:
     void init();
     void writeText(QString text, int color);
     QProcess *initProcess();
-    void activarBotones(int idx, bool b);
+    void activarBotones(int id, bool b);
     bool saveToDisk(const QString &filename, QIODevice *data);
     QString saveFileName(const QUrl &url);
-    bool validarCampos(int idx);
+    bool validarCampos(int id);
     void setConfigProgressBar(QProgressBar *pg, bool bp=true, int min=0, int max=99);
     void addToHistorial(QStringList data);
-    void CursorCarga(bool b, int idx);
+    void CursorCarga(bool b, int id);
     void checkearSalida(QStringList arg);
     void mkdirTemp(bool f, QString dir);
     bool buscarLB(QString lbase, QString *tmpA, QSettings *config);
     void getExtensiones(QStringList lt);
+    bool saveToHistory(QString filePath, QStringList data);
+    bool loadHistoryFile(QString filePath, QTableWidget *tabla);
 public slots:
     void update_Geometry();
     void imprimirSalida(QStringList lista);
@@ -55,6 +59,8 @@ public slots:
     void mostrarSalidaDM(bool b, QString s);
     void MsgBlOut(QString msg, QPlainTextEdit *QP);
     void CheckoutDIM();
+    void loadWebReport();
+    void sceneTabRemove_slot(int index);
 private slots:
     void on_actionStart_triggered();
     void on_btnSetear_clicked();
@@ -68,9 +74,13 @@ private slots:
     void on_actionProxy_Settings_triggered();
     void on_btnFormato_clicked();
     void on_btnIniciarAct_clicked();
-
     void on_actionDoTest_triggered();
+    void on_btnReloadBL_clicked();
 
+    void on_btnReloadR_clicked();
+
+signals:
+    informeTerminado();
 private:
     Ui::MainWindow *ui;
     QString m_sSettingsFile;
@@ -80,8 +90,6 @@ private:
     QList<QProcess*> listadeProcesos;
     QMediaPlayer *player;
     int cvolume = 50;
-
-    QString output_baseline_dm;
 
     const QString UPDATER = QString("cmd");
     const QStringList ARGUMENTS = ( QStringList()<<"" );
@@ -94,18 +102,23 @@ private:
     QString object_id ;
     QString area_id ;
 
+    /* Variables de salida proceso ChdckOutBaseline */
     QString BASE_URL = "http://checking:8080/report/qaking/Dimensions/";
+    QMap<QString, QString> mapURLs;
+    QString output__dm;
+    int idx; //indice de la pestaña actual a la que se le aplicara el analisis
+
 
     /* Listas de extensiones que analisa chk-qa */
     QStringList LIST_PLSQL = ( QStringList()<<".sf"<<".sps"<<".spb"<<".sp"<<".fnc"<<".spp"<<".plsql"<<".trg"<<".sql"<<".st"<<".prc"<<".pks"<<".pkb"<<".pck"<<".vw" );
+    QStringList LIST_JAVA = ( QStringList()<<".java" );
+    QStringList LIST_ACTIONSCRIPT = ( QStringList()<<".as" );
+    QStringList LIST_COBOL = ( QStringList()<<".cob"<<".cbl"<<".cpy"<<".pco" );
     QStringList LIST_ASP = ( QStringList()<<".asp" );
     QStringList LIST_JSP = ( QStringList()<<".jsp"<<".jspx"<<".xhtml" );
-    QStringList LIST_COBOL = ( QStringList()<<".cob"<<".cbl"<<".cpy"<<".pco" );
-    QStringList LIST_ACTIONSCRIPT = ( QStringList()<<".as" );
-    QStringList LIST_JAVA = ( QStringList()<<".java" );
     QStringList LIST_JAVASCRIPT = ( QStringList()<<".js" );
 
-    QStringList informes;
+    QStringList tecn_names = { "plsql", "java", "actionscript", "cobol", "asp", "jsp", "javascript" };
 };
 
 #endif // MAINWINDOW_H
